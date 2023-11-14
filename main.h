ae694bf18ffcc82b9a99f53119e129cdbcd30267
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-11-14 04:35:53 -0800

    resolved conflicts in main.h

--- main.h
+++ main.h
@@ -10,12 +10,7 @@ int my_putchar(const char c);
 int my_puts(char *s);
 int _printf(const char *format, ...);
 int print_37(void);
-<<<<<<< HEAD
 int print_number(int n);
 int _isdigit(int c);
 
-=======
-int handle_spec(va_list args, const char *for_mat);
-int print_bin(va_list var);
->>>>>>> 85bbbe7e9c3e2e9f1046bb4347fc480df7c60ce3
 #endif
